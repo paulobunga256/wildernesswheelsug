ec8bfac20e16adfd682e8357a840be4890e544c6
Paul Obunga <paulobunga.one@gmail.com>
2024-12-10 16:20:16 +0300

    feat(map): replace mapbox with google maps embed

--- src/pages/ContactMap.tsx
+++ src/pages/ContactMap.tsx
@@ -1,30 +1,17 @@
-import { useState } from "react";
-import Map, { Marker } from "react-map-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
-import { MapPin } from "lucide-react";
+import React from "react";
 
 const ContactMap = () => {
-  const [viewport, setViewport] = useState({
-    latitude: 0.3476,
-    longitude: 32.5825,
-    zoom: 12,
-    width: "100%",
-    height: "100%",
-  });
-
   return (
     <div className="h-64 bg-slate-200 rounded-xl overflow-hidden">
-      <Map
-        initialViewState={viewport}
-        style={{ width: "100%", height: "100%" }}
-        mapStyle="mapbox://styles/mapbox/streets-v11"
-        mapboxAccessToken="pk.eyJ1IjoicGF1bG9idW5nYSIsImEiOiJjbDNvYjBzc3QwMWV1MmtzZmtteDBlMzU5In0.7PvZp41vMybUUOdpCzhyaA"
-        onMove={(evt) => setViewport({ ...evt.viewState, width: "100%", height: "100%" })}
-      >
-        <Marker latitude={0.3476} longitude={32.5825} anchor="bottom">
-          <MapPin className="w-6 h-6 text-red-600" />
-        </Marker>
-      </Map>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3989.791214358317!2d32.55939865112305!3d0.21119999885559082!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1794c523601f%3A0x8277041e907c4b81!2sBooking%20Hospitality%20Leisure%20Infra%20LLP!5e0!3m2!1sen!2sug!4v1733836443657!5m2!1sen!2sug"
+        width="100%"
+        height="100%"
+        style={{ border: 0 }}
+        allowFullScreen
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
     </div>
   );
 };
